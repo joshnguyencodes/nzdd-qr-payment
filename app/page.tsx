4245d254c11499433942d357786b55e60265eb0b
Henri <henrichemla@outlook.com>
2025-03-22 16:55:41 +1300

    Added a link to the qr code on the home page

--- app/page.tsx
+++ app/page.tsx
@@ -40,7 +40,26 @@ export default function App() {
     <div className="flex flex-col min-h-screen font-sans dark:bg-background dark:text-white bg-white text-black">
       <header className="pt-4 pr-4">
         <div className="flex justify-end">
-          <div className="wallet-container">
+          <div className="flex wallet-container">
+
+            <a
+                  href="/transaction/"
+                  className="
+                    inline-block
+                    px-4
+                    py-2
+                    rounded
+                    bg-blue-500
+                    text-white
+                    font-semibold
+                    hover:bg-blue-600
+                    transition-colors
+                    duration-200
+                    margin-right-100
+                  "
+                >
+                  Create your QR code
+            </a>
             <Wallet>
               <ConnectWallet>
                 <Avatar className="h-6 w-6" />
